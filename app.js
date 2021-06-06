068f2dc5aac980dfc2ff5ed218aff9f699903f96
LEESANGMIN <sangmin4208@gmail.com>
2021-06-06 19:43:04 +0900

    Build a router

--- app.js
+++ app.js
@@ -2,33 +2,51 @@ const ajax = new XMLHttpRequest()
 const NEWS_URL = `https://api.hnpwa.com/v0/news/1.json`
 const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`
 const container = document.getElementById('root')
-const content = document.createElement('div')
 
 function getData(url) {
   ajax.open('get', url, false)
   ajax.send()
   return JSON.parse(ajax.response)
 }
-const newsFeed = getData(NEWS_URL)
-const ul = document.createElement('ul')
 
-window.addEventListener('hashchange', () => {
-  const id = location.hash.slice(1)
-  const newsContent = getData(CONTENT_URL.replace('@id', id))
-  const title = document.createElement('h1')
-  title.innerHTML = newsContent.title
-  content.appendChild(title)
-})
-
-for (let i = 0; i < newsFeed.length; i++) {
-  const div = document.createElement('div')
-  div.innerHTML = `
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL)
+  const newsList = [`<ul>`]
+  for (let i = 0; i < newsFeed.length; i++) {
+    newsList.push(`
   <li> 
     <a href=#${newsFeed[i].id}>${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
   </li>
-  `
-  ul.appendChild(div.firstElementChild)
+  `)
+  }
+  newsList.push(`</ul>`)
+  container.innerHTML = newsList.join('')
 }
 
-container.appendChild(ul)
-container.appendChild(content)
+function newsDetail() {
+  const id = location.hash.slice(1)
+  const newsContent = getData(CONTENT_URL.replace('@id', id))
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1> 
+    <div>
+      <a href="#"> 목록으로 </a>
+    </div>
+    `
+}
+
+function router() {
+  const routhPath = location.hash
+
+  switch (routhPath) {
+    case '':
+      newsFeed()
+      break
+    default:
+      newsDetail()
+  }
+}
+
+window.addEventListener('hashchange', router)
+
+router()
