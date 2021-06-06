3befa4c1aeb8c0be4e40e37eb1cbfb8b6ab1b076
LEESANGMIN <sangmin4208@gmail.com>
2021-06-06 19:12:19 +0900

    Refactoring code (DOM api, make getData function)

--- app.js
+++ app.js
@@ -3,30 +3,31 @@ const NEWS_URL = `https://api.hnpwa.com/v0/news/1.json`
 const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`
 const container = document.getElementById('root')
 const content = document.createElement('div')
-ajax.open('get', NEWS_URL, false)
-ajax.send()
 
-const newsFeed = JSON.parse(ajax.response)
+function getData(url) {
+  ajax.open('get', url, false)
+  ajax.send()
+  return JSON.parse(ajax.response)
+}
+const newsFeed = getData(NEWS_URL)
 const ul = document.createElement('ul')
 
 window.addEventListener('hashchange', () => {
   const id = location.hash.slice(1)
-
-  ajax.open('get', CONTENT_URL.replace('@id', id), false)
-  ajax.send()
-  const newsContent = JSON.parse(ajax.response)
+  const newsContent = getData(CONTENT_URL.replace('@id', id))
   const title = document.createElement('h1')
   title.innerHTML = newsContent.title
   content.appendChild(title)
 })
-for (let i = 0; i < newsFeed.length; i++) {
-  const li = document.createElement('li')
-  const a = document.createElement('a')
-  a.href = `#${newsFeed[i].id}`
-  a.innerText = `${newsFeed[i].title} (${newsFeed[i].comments_count})`
 
-  li.appendChild(a)
-  ul.appendChild(li)
+for (let i = 0; i < newsFeed.length; i++) {
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <li> 
+    <a href=#${newsFeed[i].id}>${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+  </li>
+  `
+  ul.appendChild(div.firstElementChild)
 }
 
 container.appendChild(ul)
