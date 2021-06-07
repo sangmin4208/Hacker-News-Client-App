e7ba1b0f6330460f61b1a67db3bac0708513f738
sangmin <sangmin4208@gmail.com>
2021-06-07 15:44:21 +0900

    Refectoring code using template

--- app.js
+++ app.js
@@ -15,25 +15,34 @@ function getData(url) {
 
 function newsFeed() {
   const newsFeed = getData(NEWS_URL)
-  const newsList = [`<ul>`]
+  const newsList = []
+  let template = `
+  <div class="container mx-auto p-4">
+    <h1>Hacker News</h1>
+    <ul> 
+      {{__news_feed__}}
+    </ul>
+    <div>
+      <a href="#/page/{{__prev_page__}}">이전페이지</a>
+      <a href="#/page/{{__next_page__}}">다음페이지</a>
+     </div>
+  </div>
+`
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-  <li> 
-    <a href=#/show/${newsFeed[i].id}>${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-  </li>
+      <li> 
+        <a href=#/show/${newsFeed[i].id}>${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+      </li>
   `)
   }
-  newsList.push(`</ul>`)
-  newsList.push(`
-  <div>
-    <a href="#/page/${
-      store.currentPage - 1 < 1 ? 1 : store.currentPage - 1
-    }">이전페이지</a>
-    <a href="#/page/${store.currentPage + 1}">다음페이지</a>
-  </div>
-  
-  `)
-  container.innerHTML = newsList.join('')
+  template = template.replace('{{__news_feed__}}', newsList.join(''))
+  template = template.replace(
+    '{{__prev_page__}}',
+    store.currentPage - 1 === 0 ? 1 : store.currentPage - 1
+  )
+  template = template.replace('{{__next_page__}}', store.currentPage + 1)
+
+  container.innerHTML = template
 }
 
 function newsDetail() {
